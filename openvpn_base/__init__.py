67a88b4ac2797102f825cd5dc33276f04430a6ef
Matteo Vitali <trottomv@gmail.com>
2018-08-03 07:37:02 +0200

    verbose name app

---
+++ openvpn_base/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'openvpn_base.apps.MyAppConfig'
---
+++ openvpn_base/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class MyAppConfig(AppConfig):
+  name = 'openvpn_base'
+  verbose_name = "Open VPN mon"
